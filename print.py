8e32209d1951fd5966921ff2f67561dd2b94e25b
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 11:21:10 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,17 @@
+#basics
+#to print a statement string
+print('Hello buddies')
+print("Am also a challenger for python 75")
+#intialize a variable t
+t=10
+#to print variable value just put the variable name in print statement
+print(t)
+#to print a statement that is string with variable
+print("the value of t is",t)
+#to intialize some spaces use sep 
+print("t =", t, sep="  ")
+#always print statement use new line
+#to continue print statement on the same line use end equals to space
+print("hello" ,end=" ")
+print("this is python 3.6")
+
