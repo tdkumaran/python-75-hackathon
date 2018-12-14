ffc0e12e6c1e67f05a7517ef58286a4849e9d8e8
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 21:15:10 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#dictionary
+d={}
+#to create empty dictionary
+#the keys also be a string
+d={"firstperson":"kumaran","secondperson":"ragul"}
+print(d["secondperson"])
+#to update dictionary
+d["secondperson"]="linkesh"
+print(d["secondperson"])
