955cfa4cfa23a01ca335d5294a2eaa265bb8aa6d
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 12:19:31 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+#single quotes
+print('little boy')
+#to escape the single quotes
+print('this can\'t be reached')
+#for new line use \n
+print("This can\'t be happen soon \n So leave that")
+#printing some path use r before string to consider everything as string
+#see difference
+print("user\name")
+#r used makes consider as raw string
+print(r"user\name")
+
+
