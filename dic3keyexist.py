339aa9c029caf34db30af10676dda64fb3329202
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 21:16:27 +0530

    Add files via upload

---
+++ dic3keyexist.py
@@ -0,0 +1,15 @@
+d={"country":'india',"district":'madurai',"pin":623}
+print(d)
+#check the key district in dictionary d
+if "district" in d:
+    print("distrcit")
+#presence make a print
+for x in d:
+    print(d[x])
+#print all the values in the dictionary
+for x in d:
+    print(d.keys())
+#print all the keys in the dictionary
+for x in d.items():
+    print(x)
+#items funtion used to print all the key and values in the dictionary
