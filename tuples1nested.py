9d54181a4aa6b8cd85d7cd76e867aede3e0aefef
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 20:22:29 +0530

    Add files via upload

---
+++ tuples1nested.py
@@ -0,0 +1,9 @@
+#nested
+a=1,2,3,4
+print(a)
+#the above created tuple is used in b
+b=a,(5,6,7),9
+print(b)
+#nested tuple b
+c=b,(20)
+print(c)
---
+++ tuples4delslice.py
@@ -0,0 +1,16 @@
+tup=1,2,3,4,55
+print(tup)
+#printed tuple
+
+#it gives an error
+tup1=2,3,4,5,6,7
+print(tup1[2:4])
+print(5 in tup)
+#to check availability returns with boolean value
+print(4 in tup)
+#check 4
+del tup
+#delted tuple
+print(tup)
+#now printing gives an error
+
