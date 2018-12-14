e8449c6bba31dafe5f782f380aa49fd08b24ac04
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-14 21:16:06 +0530

    Add files via upload

---
+++ dic2clear.py
@@ -0,0 +1,7 @@
+d={"number":124, "name":'ram', "roomnum":24}
+print(d["roomnum"],d["number"],d["name"])
+#all values are accessable
+d.clear()
+#it clear all the values
+print(d["name"])
+#gives an error
