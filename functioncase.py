8931fd8317ff21e212eeb316097d3aa214e735bd
tdkumaran <tdkumaran99@gmail.com>
2018-12-15 13:50:46 +0530

    Check cases

--- functioncase.py
+++ functioncase.py
@@ -3,8 +3,10 @@ def givenstring(s):
     d={"Uppercase":0,"Lowercase":0}
     for x in s:
         if x.isupper():
+	#check uppercase
             d["Uppercase"]+=1
         if x.islower():
+	#check lowercase
             d["Lowercase"]+=1
     print("Original String",s)
     print("No of uppercase",d["Uppercase"])
