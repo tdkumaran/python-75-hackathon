d0357dfe312b99ceeba0e3ce2b1c2557eedaed90
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-15 09:29:20 +0530

    Add files via upload

---
+++ globalnonlocal.py
@@ -0,0 +1,12 @@
+def fun():
+    x="local variable"
+    #local variable defined
+    def fun1():
+        nonlocal x
+        #in nested nonlocal variable defined as x, any changes affect the local
+        x="nonlocal variable"
+        print("Function1 inner values is",x)
+    fun1()
+    print("Function outside variable",x)
+fun()
+#both print nonlocal
