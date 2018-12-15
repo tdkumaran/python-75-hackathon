8728d0f968522f15cd95c71ff68f56b257bdc63d
tdkumaran <30695672+tdkumaran@users.noreply.github.com>
2018-12-15 09:28:59 +0530

    Add files via upload

---
+++ globallocalpassbyref.py
@@ -0,0 +1,7 @@
+def passbyref(mylist):
+    for x in range(3):
+        mylist.append(x)
+    print("Inisde funciton list values",mylist)
+mylist=[5,6,7]
+print("Outside funtion list values",mylist)
+passbyref(mylist)
