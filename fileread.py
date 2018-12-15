064c84fbf5e3b5bf94f9501dcbf3ff6e5aa4e68e
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 00:58:04 +0530

    Read file

---
+++ fileread.py
@@ -0,0 +1,6 @@
+file=open("C:/Users/KUMARAN/Desktop/python-75-hackathon/variable.py","r")
+#open the file in the read mode
+contents=file.read()
+#it read the entire things in the file and store in the contents
+print(contents)
+#now printing contents write everything in the output
