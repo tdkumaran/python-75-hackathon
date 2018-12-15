5dd4d525b14f5f69445c0cab2e5b9ab568cb9d26
tdkumaran <tdkumaran99@gmail.com>
2018-12-15 13:20:33 +0530

    Randomnum newline

---
+++ randomnum.py
@@ -0,0 +1,7 @@
+import random
+#import random to generate random number
+for x in range(10):
+    #generate 10 random number
+    print(random.randint(1,20)*10)
+    #generated every number is multiplied by 10
+    #hello
