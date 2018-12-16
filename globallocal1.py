8cfd9b1ae0491ace5aa86ddb73c11c74301dd904
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:58:26 +0530

    delete

--- globallocal1.py
+++
@@ -1,9 +0,0 @@
-#global vs local
-def mul(x,y):
-    op=x*y
-    print("The value of op inside function is",op)
-op=100
-x=int(input("Enter x"))
-y=int(input("Enter y"))
-print("Tha value of op outside function is",op)
-mul(x,y)
