f2c55d6042ab54fc619cb4166b2dab43059e1580
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:57:07 +0530

    delete

--- randomcolor.py
+++
@@ -1,9 +0,0 @@
-import random
-#importing random
-n=int(input("Enter the number of color to be in"))
-for x in range(n):
-    list=[]
-    str=input("Color:\r")
-    list.append(str)
-print("The random color picked is",random.choice(list))
-#choosing a random color
