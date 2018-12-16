ac4a62890c9ecba65373561a0588eca58a08e72a
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:15:51 +0530

    commit

--- filerenameremove.py
+++
@@ -1,5 +0,0 @@
-import os
-os.rename("new.txt", "new10.txt")
-#changing the name of the file named with new.txt to new10.txt
-os.remove("new1.txt")
-#it remove or delete the file named new1.txt
--- writefile.py
+++
@@ -1,7 +0,0 @@
-#file open with automatically flose when operation overs
-with open('new.txt','w') as f:
-    f.write("Again come with other words using with\n")
-    f.write("Only 2 line is enought\n")
-with open('new.txt','a') as f:
-    f.writelines(['hello\n','this is another two line'])
-        
