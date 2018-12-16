e2205da0bd3e241277a8372a05f9b7d5e4bc81d8
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:43:41 +0530

    range fun

---
+++ Range function/functionrange.py
@@ -0,0 +1,16 @@
+#range function
+for x in range(10):
+#range upto 10 comes
+    print(x)
+#print upto number 9 starting from 0
+for x in range(10,20):
+#print numbers from 10 to 19
+    print(x)
+for x in range(0,10,3):
+#first argument starting point
+#second argument ending point
+#third argument increment
+    print(x)
+#using range function we can also create list
+print(list(range(10)))
+#list is created starting index from 0 to 9
