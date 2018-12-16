59f0e2da4f8721946b81f224e04b7c94dcf55d22
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 19:01:26 +0530

    de

--- filer+.py
+++
@@ -1,6 +0,0 @@
-with open("new7.txt","r+") as f:
-    f.seek(0,0)
-    f.write("This is new writing")
-    #able to use both read and write operation
-    print(f.read())
-    #read operation is performed
