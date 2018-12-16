703970292654d15fef25333824a5f0b8e5475b50
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 19:00:06 +0530

    d

--- filereadforloop.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the number of lines to be printed:"))
-f=open("C:/Users/KUMARAN/Desktop/python-75-hackathon/variable.py",'r')
-print(f.tell())
-for i in range(n):
-        print(f.readline(),end=' ')
-print(f.tell())
-print(f.seek(50))
-print(f.tell())
-print(f.read(50))
--- functiondef.py
+++
@@ -1,6 +0,0 @@
-def even(n):
-    if(n%2==0):
-        print("The output is even number",n)
-    else:
-        print("The output is odd number",n)
-fact(3)
