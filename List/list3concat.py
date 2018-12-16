4b2c50cc5930f27c5c5c950bbac9c61bae4fe46b
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:48:25 +0530

    List operations

---
+++ List/list3concat.py
@@ -0,0 +1,11 @@
+#list concat
+list1=[1,2,3,4,5]
+list2=[6,7,8,9]
+#for concat use +
+list3=list1+list2
+print(list3)
+list3=list3+[10]
+print(list3)
+#for check two list names pointing same
+print(list2== list1)
+#returns boolean values
---
+++ List/list4compre.py
@@ -0,0 +1,18 @@
+#list comprehensions
+list1=[1,2,3,4,5]
+def iseven(x):
+    return(x%2==0)
+def square(x):
+    return(x*x)
+print(list(map(square,list1)))
+#map applies some function to every element in the list
+evenlist=[]
+for x in list1:
+    if iseven(x):
+        evenlist.append(x)
+print(evenlist)
+#created evenlist as sublist
+print([square(x) for x in list1 if iseven(x)])
+#mapping first one that applies function
+#generator that is range taking element
+#filter filter that with some condition
