2a21191525235b8b3560df07e4f5842336fe9697
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 19:02:33 +0530

    d

--- dic2clear.py
+++
@@ -1,8 +0,0 @@
-#dictionary clear
-d={"number":124, "name":'ram', "roomnum":24}
-print(d["roomnum"],d["number"],d["name"])
-#all values are accessable
-d.clear()
-#it clear all the values
-print(d["name"])
-#gives an error
--- dictionary.py
+++
@@ -1,9 +0,0 @@
-#create dictionary
-d={}
-#to create empty dictionary
-#the keys also be a string
-d={"firstperson":"kumaran","secondperson":"ragul"}
-print(d["secondperson"])
-#to update dictionary
-d["secondperson"]="linkesh"
-print(d["secondperson"])
