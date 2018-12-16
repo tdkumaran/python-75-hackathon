68562ad40a872dcb963805ef9ddbc65c4205947e
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:49:34 +0530

    String operations

---
+++ String/stroperation.py
@@ -0,0 +1,15 @@
+#to concatenate strings
+prefix="talent"
+suffix="accurate"
+output=prefix+suffix
+print(output)
+#to repeate string multiple times 2 or 3
+print(output*3)
+#indexing
+#to take character at first position (whatever position we want)
+print(output[0])
+print(output[10])
+#to take character from last position use -1 -2
+print(suffix[-1])
+print(suffix[-5])
+
