1d18de4a1c00be182045c7866fa91567df22607c
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:37:20 +0530

    dir function

---
+++ dir()/strmodule.py
@@ -0,0 +1,6 @@
+def strings(s):
+    print("Ey there"+ s + "nice to welcome to python hackathon")
+def descriptions(s):
+    print("hello mr." + s +"You want to know further details contact")
+def rules(s):
+    print("There are certain rules there"  +s+" please follow that")
