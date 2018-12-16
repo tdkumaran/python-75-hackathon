624d22194682ba9e9d34031bddd87e8490aa2456
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:50:34 +0530

    tuples use

---
+++ tuples/tuples.py
@@ -0,0 +1,15 @@
+#create tuples
+tup=1,2,3,4
+print(tup)
+#simultaneous assignment
+(myname,mycountry,mynumber)=('abc','india',234)
+print(myname)
+
+points=3.5,2.3
+print(points[0])
+
+date=14,12,2018
+print(date[1:])
+#tuples are immutable
+#date[1]=8 gives an error
+
