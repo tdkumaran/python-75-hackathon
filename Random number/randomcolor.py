995542deb6990167fedf6855ae39e1504492e0f4
tdkumaran <tdkumaran99@gmail.com>
2018-12-16 18:45:53 +0530

    Create random

---
+++ Random number/randomcolor.py
@@ -0,0 +1,9 @@
+import random
+#importing random
+n=int(input("Enter the number of color to be in"))
+for x in range(n):
+    list=[]
+    str=input("Color:\r")
+    list.append(str)
+print("The random color picked is",random.choice(list))
+#choosing a random color
